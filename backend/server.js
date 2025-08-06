6ce98f4969050263c69209918959df595c6881e0
delta-student <136906596+vs7174379@users.noreply.github.com>
2025-08-06 19:03:13 +0530

    new commit

--- backend/server.js
+++ backend/server.js
@@ -20,10 +20,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
-app.use(cors({
-  origin: 'https://cinemo-xnb9.vercel.app/', // or your frontend URL/port
-  credentials: true
-}));
+app.use(cors());
 
 // Routes
 app.use('/api/show', showRouter);
