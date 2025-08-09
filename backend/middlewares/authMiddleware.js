46b6b6f616a08db3bef77f09728a8a8d0a9b6680
delta-student <136906596+vs7174379@users.noreply.github.com>
2025-08-09 22:46:46 +0530

    new commit

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -4,7 +4,10 @@ import { User } from "../models/user.js";
 export const authMiddleware = async (req, res, next) => {
   try {
     const token = req.cookies.token;
-    if (!token) return res.status(401).json({ message: "Not authorized" });
+    if (!token){
+        console.error("No token provided");
+        return res.status(401).json({ message: "Unauthorized access", success: false });
+    }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
