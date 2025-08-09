3088de41d8dcc2c08f12626a8e77ff4821b50dfb
delta-student <136906596+vs7174379@users.noreply.github.com>
2025-08-09 20:03:48 +0530

    new commit

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from "dotenv";
-dotenv.config({ quiet: true });
+dotenv.config();
 import connectDB from './configs/db.js';
 import showRouter from './routes/showRoutes.js';
 import userRouter from './routes/user.js';
@@ -10,10 +10,6 @@ import { authMiddleware } from './middlewares/authMiddleware.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-
-
-
 
 // Connect to DB
 (async () => {
@@ -30,37 +26,49 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-
-
 // CORS Configuration
-const corsOptions = {
-    origin: 'https://cinemo-5p8g.vercel.app',
-    credentials: true
-}
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://cinemo-5p8g.vercel.app',
+    'https://cinemo-ashy.vercel.app'
+];
 
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
+};
 
 app.use(cors(corsOptions));
 
-
 // Routes
 app.use('/api/show', showRouter);
-app.use('/api/user', userRouter);
+app.use('/api/user', authMiddleware, userRouter); // Apply authMiddleware to /api/user routes
 
 // Root route
 app.get('/', (req, res) => res.send('Server is Live!'));
-app.get("/profile", authMiddleware, (req, res) => {
+
+app.get("/api/profile", authMiddleware, (req, res,next) => {
   res.json({ user: req.user });
+  next()
 });
 
 // 404 handler
 app.use((req, res, next) => { // Added next
     res.status(404).json({ message: 'Resource not found', success: false }); // Added success: false
+    next()
 });
 
 // Error handler
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ success: false, message: 'Internal Server Error' });
+    next()
 });
 
 // Start server
