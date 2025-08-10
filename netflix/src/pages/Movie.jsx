7657433bfd62991e8316ab979382fcbdb3c812f6
delta-student <136906596+vs7174379@users.noreply.github.com>
2025-08-10 23:47:37 +0530

    new commit

--- netflix/src/pages/Movie.jsx
+++ netflix/src/pages/Movie.jsx
@@ -78,7 +78,7 @@ const Movie = () => {
               {movie.genre }
             </div>
             <div className="flex flex-col sm:flex-row gap-4 mb-4">
-             <Link to={`/movies/${movie._id}/watch`}>
+             <Link to={`/movies/${id}/watch`}>
 
               <button className="bg-white text-black font-semibold px-6 py-3 rounded flex items-center gap-2 text-lg">
                 <i className="fas fa-play" /> Watch now
