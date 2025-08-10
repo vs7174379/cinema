913270beba180a46cc0f7d5401b7c4aeecb9154e
delta-student <136906596+vs7174379@users.noreply.github.com>
2025-08-10 07:19:43 +0530

    new commit

--- netflix/src/components/MovieContainer.jsx
+++ netflix/src/components/MovieContainer.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import {comedy, romantic, top, womenrule} from '../../lib/data';
+import { comedy, romantic, top, womenrule } from '../../lib/data';
 
 
 import MovieList from './MovieList';
@@ -8,6 +8,23 @@ import axios from "axios"
 const MovieContainer = () => {
     const [movies, setMovies] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [movie, setMovie] = useState([]);
+    useEffect(() => {
+        const fetchMovies = async () => {
+            try {
+                const response = await axios.get('https://cinemo-ashy.vercel.app/api/show/movie');
+                const data = response.data.movies
+                if (response.data.success) {
+                    setMovie(data);
+                } else {
+                    console.error('Failed to fetch movies:', data.message);
+                }
+            } catch (error) {
+                console.error('Error fetching movies:', error);
+            }
+        }
+        fetchMovies();
+    }, []);
 
     useEffect(() => {
         const fetchNowPlaying = async () => {
@@ -29,21 +46,22 @@ const MovieContainer = () => {
     if (loading) return <p className="text-center text-white">Loading...</p>;
 
     return (
-         <div>
-             <MovieList title={"you might like"} movies={top}/>
-             <MovieList title={"Comedy Movies"} movies={comedy}/>
-             <MovieList title={"Romantic Movies"} movies={romantic}/>
-             <MovieList title={"Women Ruling Movies"} movies={womenrule}/>
+        <div>
+            <MovieList title={"you might like"} movies={top} />
+            <MovieList title={"Comedy Movies"} movies={comedy} />
+            <MovieList title={"Romantic Movies"} movies={romantic} />
+            <MovieList title={"Women Ruling Movies"} movies={womenrule} />
+            <MovieList title={"Movies"} movies={movie} />
 
 
-         </div>
+        </div>
 
-       
-        
 
-       
-        
-    
+
+
+
+
+
     );
 };
 export default MovieContainer
