99ecc8319808c8c17f71ee8687aa26bfbbaf0af9
delta-student <136906596+vs7174379@users.noreply.github.com>
2025-08-10 08:09:43 +0530

    new commit

--- netflix/src/components/MovieContainer.jsx
+++ netflix/src/components/MovieContainer.jsx
@@ -10,8 +10,31 @@ const MovieContainer = () => {
     const [loading, setLoading] = useState(true);
     const [movie, setMovie] = useState([]);
     const top = movie.filter(m =>
-        m.type === 'top indian movies'
+        m.type === 'romantic indian movies'
     );
+    const comedyMovies = movie.filter(m =>
+        m.type === 'top comedy indian movies'
+    );
+    const marvel= movie.filter(m =>
+        m.type === 'marvel movies'
+    );
+    const emotional = movie.filter(m =>
+        m.type === 'top emotional indian movies'
+    );
+    const south = movie.filter(m =>
+        m.type === 'top south indian movies'
+    );
+    const thriller = movie.filter(m =>
+        m.type === 'top thriller indian movies'
+    );
+    const hollywood = movie.filter(m =>
+        m.type === 'hollywood movies'
+    );
+    const hollywoodRomantic = movie.filter(m =>
+        m.type === 'hollywood romantic movies'
+    );
+
+        
 
 
     useEffect(() => {
@@ -53,10 +76,14 @@ const MovieContainer = () => {
     return (
         <div>
             <MovieList title={"you might like"} movies={top} />
-            <MovieList title={"Comedy Movies"} movies={comedy} />
-            <MovieList title={"Romantic Movies"} movies={romantic} />
-            <MovieList title={"Women Ruling Movies"} movies={womenrule} />
-            <MovieList title={"Movies"} movies={movie} />
+            <MovieList title={"Comedy Movies"} movies={comedyMovies} />
+            <MovieList title={"Marvel-Hits"} movies={marvel} />
+            <MovieList title={"Emotional-Movies"} movies={emotional} />
+            <MovieList title={"Tollywood-BlockBusters"} movies={south} />
+            <MovieList title={"Thriller-Movies"} movies={thriller} />
+            <MovieList title={"Hollywood-Movies"} movies={hollywood} />
+            <MovieList title={"Hollywood-Romantic-Movies"} movies={hollywoodRomantic} />
+            
 
 
         </div>
--- netflix/src/pages/MyList.jsx
+++ netflix/src/pages/MyList.jsx
@@ -1,86 +1,86 @@
 import { Play } from 'lucide-react';
+import { useEffect, useState } from 'react';
 
-const continueWatchingData = [
-  {
-    title: "King of Kotha",
-    time: "2h 43m left",
-    image: "https://media-cache.cinematerial.com/p/500x/qy40tpve/king-of-kotha-indian-movie-poster.jpg?v=1733437134", // replace with actual poster URL
-    lang: "Malayalam"
-  },
-  {
-    title: "Humraaz",
-    time: "2h 38m left",
-    image: "https://m.media-amazon.com/images/M/MV5—Humraaz_poster.jpg"
-  },
-  {
-    title: "Hai Junoon!",
-    time: "26m left",
-    image: "https://via.placeholder.com/300x170",
-    season: "S1 E1"
-  },
-  {
-    title: "Shadaa",
-    time: "1h 36m left",
-    image: "https://via.placeholder.com/300x170"
-  },
-  {
-    title: "Kull",
-    time: "29m left",
-    image: "https://via.placeholder.com/300x170",
-    season: "S1 E1"
-  },
-  {
-    title: "Game of Greed",
-    time: "36m left",
-    image: "https://via.placeholder.com/300x170",
-    season: "S1 E1"
-  },
-  {
-    title: "Mere Husband Ki Biwi",
-    time: "2h 3m left",
-    image: "https://via.placeholder.com/300x170"
-  },
-  {
-    title: "Saba Nayagan",
-    time: "2h 16m left",
-    image: "https://via.placeholder.com/300x170",
-    lang: "Hindi"
-  },
-  {
-    title: "Sweetheart!",
-    time: "2h 14m left",
-    image: "https://via.placeholder.com/300x170",
-    lang: "Hindi"
-  },
-  {
-    title: "The Legend of Hanuman",
-    time: "16m left",
-    image: "https://via.placeholder.com/300x170",
-    season: "S6 E1"
-  }
-];
 
-export default function MyList({head}) {
+
+export default function MyList({ head }) {
+  const [movie, setMovie] = useState([]);
+  const top = movie.filter(m =>
+    m.type === 'romantic indian movies'
+  );
+  const comedyMovies = movie.filter(m =>
+    m.type === 'top comedy indian movies'
+  );
+  const marvel = movie.filter(m =>
+    m.type === 'marvel movies'
+  );
+  const emotional = movie.filter(m =>
+    m.type === 'top emotional indian movies'
+  );
+  const south = movie.filter(m =>
+    m.type === 'top south indian movies'
+  );
+  const thriller = movie.filter(m =>
+    m.type === 'top thriller indian movies'
+  );
+  const hollywood = movie.filter(m =>
+    m.type === 'hollywood movies'
+  );
+  const hollywoodRomantic = movie.filter(m =>
+    m.type === 'hollywood romantic movies'
+  );
+
+
+
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await axios.get('https://cinemo-ashy.vercel.app/api/show/movie');
+        const data = response.data.movies
+        if (response.data.success) {
+          setMovie(data);
+        } else {
+          console.error('Failed to fetch movies:', data.message);
+        }
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    }
+    fetchMovies();
+  }, []);
+
+  useEffect(() => {
+    const fetchNowPlaying = async () => {
+      try {
+        const response = await axios.get('https://cinemo-ashy.vercel.app/api/show/toprated-movies');
+        if (response.data.success) {
+          setMovie(response.data.movies);
+        }
+      } catch (error) {
+        console.error('Error fetching now playing movies:', error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchNowPlaying();
+  }, []);
   return (
     <div className="p-6  min-h-screen text-white">
       <h2 className="text-2xl font-bold mb-6">{head} </h2>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
-        {continueWatchingData.map((item, index) => (
-          <div key={index} className="space-y-2">
-            <div className="relative group">
-              <img src={item.image} alt={item.title} className="rounded-md object-cover w-full h-[170px]" />
-              <div className="absolute inset-0 flex items-center justify-center bg-black/40 opacity-0 group-hover:opacity-100 transition">
-                <Play className="w-10 h-10 text-white" />
-              </div>
-              <div className="h-1 bg-blue-600 absolute bottom-0 left-0 w-2/3"></div>
-            </div>
-            <div>
-              <p className="text-sm font-semibold">{item.season || item.title}</p>
-              {item.season && <p className="text-sm">{item.title}</p>}
-              <p className="text-xs text-gray-400">{item.time}</p>
-            </div>
-          </div>
-        ))}
+      <div>
+        <MovieList title={"Romantic Movies"} movies={top} />
+        <MovieList title={"Comedy Movies"} movies={comedyMovies} />
+        <MovieList title={"Marvel-Hits"} movies={marvel} />
+        <MovieList title={"Emotional-Movies"} movies={emotional} />
+        <MovieList title={"Tollywood-BlockBusters"} movies={south} />
+        <MovieList title={"Thriller-Movies"} movies={thriller} />
+        <MovieList title={"Hollywood-Movies"} movies={hollywood} />
+        <MovieList title={"Hollywood-Romantic-Movies"} movies={hollywoodRomantic} />
+
+
+
       </div>
     </div>
   );
