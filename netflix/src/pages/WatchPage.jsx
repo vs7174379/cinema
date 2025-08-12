745f2971f58a3649d9a8812845b51ddcc602eb66
delta-student <136906596+vs7174379@users.noreply.github.com>
2025-08-12 17:46:23 +0530

    new commit

--- netflix/src/pages/WatchPage.jsx
+++ netflix/src/pages/WatchPage.jsx
@@ -24,7 +24,17 @@ const WatchPage = () => {
   }, [id]);
 
   return (
-    <div>WatchPage</div>
+    <div>
+       <iframe
+        width="100%"
+        height="100%"
+        src={`${movie.trailerUrl}?autoplay=1&controls=1`} // Use movie.videoUrl
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
   )
 }
 
