a44c736c031f4aeabd9f36e79ca932dda7d2c1f2
delta-student <136906596+vs7174379@users.noreply.github.com>
2025-08-13 09:11:20 +0530

    new commit

--- netflix/src/components/Navbar.jsx
+++ netflix/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     useEffect(() => {
         const fetchProfile = async () => {
             try {
-                const res = await fetch(`https://cinemo-ashy.vercel.app/api/user/profile`, {
+                const res = await fetch(`https://cinema-flame-seven.vercel.app/api/user/profile`, {
                     method: "GET",
                     credentials: "include", // sends cookies
                 });
