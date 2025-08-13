4b9389e0f1aeeeabca7fdea48fbdd4f378fb5e18
delta-student <136906596+vs7174379@users.noreply.github.com>
2025-08-13 08:56:40 +0530

    new commit

--- netflix/src/Watch.jsx
+++ netflix/src/Watch.jsx
@@ -64,7 +64,7 @@ const Watch = () => {
 
 
       {/* Video player */}
-      <div className="flex-1 flex h-fit ">
+      <div className="absolute top-0 flex-1 flex h-full ">
       
         {movie.trailerUrl && movie.trailerUrl.includes("youtube") ? (
           <iframe
