e7867771bff4279b6137e2feda4d18fc941b2a6f
delta-student <136906596+vs7174379@users.noreply.github.com>
2025-08-16 18:06:00 +0530

    new commit

--- netflix/src/pages/Profile.jsx
+++ netflix/src/pages/Profile.jsx
@@ -174,7 +174,7 @@ const Profile = () => {
 
   if (loading) {
     return (
-      <div className="flex items-center justify-center h-dvh text-white">
+      <div className="flex items-center justify-center h-lvh text-white">
         <Loader />
       </div>
     );
