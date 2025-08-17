3deec114c910092c1be01e6fe00529d1b213d1c4
delta-student <136906596+vs7174379@users.noreply.github.com>
2025-08-17 12:08:58 +0530

    new commit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,6 +5,9 @@ import {
   logOut,
   updateUser,
   getProfile,
+  addToWatchlist,
+  getWatchlist,
+  removeFromWatchlist,
  
 } from '../controllers/user.js';
 import { authMiddleware } from '../middlewares/authMiddleware.js';
@@ -27,4 +30,7 @@ router.get('/profile', authMiddleware, getProfile);
 router.put('/:id', authMiddleware, updateUser);
 
 
+router.post("/add", addToWatchlist);          // Add movie
+router.get("/:userId", getWatchlist);         // Get user's watchlist
+router.post("/remove", removeFromWatchlist);
 export default router;
