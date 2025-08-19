076b27e2bb71b8d38d0f787eaf0ed147d7900d80
delta-student <136906596+vs7174379@users.noreply.github.com>
2025-08-19 16:52:10 +0530

    new commit

--- netflix/src/Watch.jsx
+++ netflix/src/Watch.jsx
@@ -149,7 +149,7 @@ const Watch = () => {
   if (!movie) return <div>Loading...</div>;
 
   return (
-    <div className="w-full py-0 bg-black">
+    <div className="w-full my-0 h-full bg-black">
       {movie.trailerUrl?.includes("youtube") ? (
         // ✅ YouTube
         <iframe
