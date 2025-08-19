dddd88e94b497d6c87ec9e4d22fcfcf54b1f4f33
delta-student <136906596+vs7174379@users.noreply.github.com>
2025-08-19 07:08:19 +0530

    new commit

--- netflix/src/pages/Movie.jsx
+++ netflix/src/pages/Movie.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 import Loader from '../components/Loader';
+import MovieList from '../components/MovieList';
 
 
 const Movie = () => {
