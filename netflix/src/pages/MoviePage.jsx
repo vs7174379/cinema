9e9f4a52adb5c527fd1c9132498987d6e9b3c1b7
delta-student <136906596+vs7174379@users.noreply.github.com>
2025-08-19 22:18:52 +0530

    new commit

--- netflix/src/pages/MoviePage.jsx
+++ netflix/src/pages/MoviePage.jsx
@@ -1,15 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
+import axios from "axios";
 
-const movies = [
-  { title: "Inception" },
-  { title: "Interstellar" },
-  { title: "The Dark Knight" },
-  { title: "Avatar" },
-];
 
 export default function MoviePage() {
+    const [movies, setMovies] = useState([]);
+    
+
+    useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await axios.get('https://cinema-flame-seven.vercel.app/api/show/movie');
+        const data = response.data.movies
+        if (response.data.success) {
+          setMovies(data);
+          
+        } else {
+          console.error('Failed to fetch movies:', data.message);
+        }
+      } catch (error) {
+        console.error('Error fetching movies:', error);
+      }
+    }
+    fetchMovies();
+  }, []);
   const [filtered, setFiltered] = useState(movies);
+
 
   const handleSearch = (query) => {
     const results = movies.filter((m) =>
