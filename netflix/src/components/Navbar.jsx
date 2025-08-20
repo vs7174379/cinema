989cc415e16ba1b032d3a5c248b23485c3758138
Vivek Sharma <136906596+vs7174379@users.noreply.github.com>
2025-08-20 08:10:09 +0530

    Update Navbar.jsx

--- netflix/src/components/Navbar.jsx
+++ netflix/src/components/Navbar.jsx
@@ -189,10 +189,10 @@ const Navbar = () => {
 
             {/* Search Overlay */}
             {showOverlay && (
-                <div className="fixed  inset-0 bg-black/60 z-[999] flex justify-center items-center py-18">
+                <div className="fixed  inset-0 bg-black/60 z-[999] flex justify-center items-center my-24">
                     <div
                         ref={overlayRef}
-                        className="bg-white py-18 rounded-lg max-h-[80vh] w-full max-w-4xl overflow-y-auto shadow-lg"
+                        className="bg-white my-24 rounded-lg max-h-[80vh] w-full max-w-4xl overflow-y-auto shadow-lg"
                     >
                         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                             {filtered.map((v, idx) => (
