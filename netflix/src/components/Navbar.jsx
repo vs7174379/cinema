00daaa628c6b42479ac29bdd68b8ecbeba6dc24a
Vivek Sharma <136906596+vs7174379@users.noreply.github.com>
2025-08-20 07:15:28 +0530

    Update Navbar.jsx

--- netflix/src/components/Navbar.jsx
+++ netflix/src/components/Navbar.jsx
@@ -1,16 +1,42 @@
 import { LogOut } from 'lucide-react';
 import React, { useState, useRef, useEffect } from 'react';
-
+import SearchBar from './SearchBar';
 import axios from 'axios';
-
+import Cards from './Cards';
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
     const [user, setUser] = useState(null);
     const menuRef = useRef(null);
-    
+    const [movies, setMovies] = useState([]);
 
 
+    useEffect(() => {
+        const fetchMovies = async () => {
+            try {
+                const response = await axios.get('https://cinema-flame-seven.vercel.app/api/show/movie');
+                const data = response.data.movies
+                if (response.data.success) {
+                    setMovies(data);
+
+                } else {
+                    console.error('Failed to fetch movies:', data.message);
+                }
+            } catch (error) {
+                console.error('Error fetching movies:', error);
+            }
+        }
+        fetchMovies();
+    }, []);
+    const [filtered, setFiltered] = useState(movies);
+
+
+    const handleSearch = (query) => {
+        const results = movies.filter((m) =>
+            m.title.toLowerCase().includes(query.toLowerCase())
+        );
+        setFiltered(results);
+    };
 
     useEffect(() => {
         const fetchProfile = async () => {
@@ -111,9 +137,13 @@ const Navbar = () => {
                 </div>
             </div>
 
-
-            
-          
+            {/* Search Bar */}
+            <SearchBar  onSearch={handleSearch} />
+            <div className="relative overflow-y-auto h-72 w-72  m-2">
+                {filtered.map((v, idx) => (
+                    <Cards video={v}/>
+                ))}
+            </div>
 
             {/* Navigation Buttons - Desktop */}
             <div className="hidden md:flex flex-wrap justify-center gap-2 text-sm text-yellow-400 font-semibold">
@@ -141,3 +171,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
