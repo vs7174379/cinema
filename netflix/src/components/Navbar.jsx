b8e57d3b1211f82a344b2e0fe65750344f9e351d
Vivek Sharma <136906596+vs7174379@users.noreply.github.com>
2025-08-22 22:54:34 +0530

    Update Navbar.jsx

--- netflix/src/components/Navbar.jsx
+++ netflix/src/components/Navbar.jsx
@@ -169,7 +169,7 @@ const Navbar = () => {
                             </div>
                         )}
                     </div>
-                </div>
+                
 
 
                 <div >
