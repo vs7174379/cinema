df991b16be28a66e158607313f4be8a47b4ee608
Vivek Sharma <136906596+vs7174379@users.noreply.github.com>
2025-08-22 23:15:05 +0530

    Update Navbar.jsx

--- netflix/src/components/Navbar.jsx
+++ netflix/src/components/Navbar.jsx
@@ -113,10 +113,49 @@ const Navbar = () => {
         <>
             <nav className="sticky top-0 z-50 bg-black/80 backdrop-blur-md flex items-center justify-between px-6 py-3 shadow-md">
                 <h1 className="text-2xl font-extrabold text-yellow-400">Cinema</h1>
+             
 
-           
 
-                    {/* Profile Dropdown */}
+                <div >
+                    <div class="search">
+                        <input value={query}
+                            onChange={handleChange}
+                            type="text"
+                            placeholder="  Search..  " class="input" />
+                        <div class="icon">
+                            <svg
+                                version="1.1"
+                                id="Capa_1"
+                                xmlns="http://www.w3.org/2000/svg"
+                                xmlns:xlink="http://www.w3.org/1999/xlink"
+                                viewBox="0 0 488.4 488.4"
+                                xml:space="preserve"
+                            >
+                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                <g
+                                    id="SVGRepo_tracerCarrier"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                ></g>
+                                <g id="SVGRepo_iconCarrier">
+                                    <g>
+                                        <g>
+                                            <path
+                                                d="M0,203.25c0,112.1,91.2,203.2,203.2,203.2c51.6,0,98.8-19.4,134.7-51.2l129.5,129.5c2.4,2.4,5.5,3.6,8.7,3.6 s6.3-1.2,8.7-3.6c4.8-4.8,4.8-12.5,0-17.3l-129.6-129.5c31.8-35.9,51.2-83,51.2-134.7c0-112.1-91.2-203.2-203.2-203.2 S0,91.15,0,203.25z M381.9,203.25c0,98.5-80.2,178.7-178.7,178.7s-178.7-80.2-178.7-178.7s80.2-178.7,178.7-178.7 S381.9,104.65,381.9,203.25z"
+                                            ></path>
+                                        </g>
+                                    </g>
+                                </g>
+                            </svg>
+                        </div>
+                        <div class="i"></div>
+                        <div class="i"></div>
+                        <div class="i"></div>
+                        <div class="i"></div>
+                        <span></span>
+                    </div>
+                </div>
+                 {/* Profile Dropdown */}
                     <div className="relative inline-block text-right" ref={menuRef}>
                         <button
                             onClick={() => setOpen(!open)}
@@ -169,48 +208,6 @@ const Navbar = () => {
                             </div>
                         )}
                     </div>
-                
-
-
-                <div >
-                    <div class="search">
-                        <input value={query}
-                            onChange={handleChange}
-                            type="text"
-                            placeholder="  Search..  " class="input" />
-                        <div class="icon">
-                            <svg
-                                version="1.1"
-                                id="Capa_1"
-                                xmlns="http://www.w3.org/2000/svg"
-                                xmlns:xlink="http://www.w3.org/1999/xlink"
-                                viewBox="0 0 488.4 488.4"
-                                xml:space="preserve"
-                            >
-                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                                <g
-                                    id="SVGRepo_tracerCarrier"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                ></g>
-                                <g id="SVGRepo_iconCarrier">
-                                    <g>
-                                        <g>
-                                            <path
-                                                d="M0,203.25c0,112.1,91.2,203.2,203.2,203.2c51.6,0,98.8-19.4,134.7-51.2l129.5,129.5c2.4,2.4,5.5,3.6,8.7,3.6 s6.3-1.2,8.7-3.6c4.8-4.8,4.8-12.5,0-17.3l-129.6-129.5c31.8-35.9,51.2-83,51.2-134.7c0-112.1-91.2-203.2-203.2-203.2 S0,91.15,0,203.25z M381.9,203.25c0,98.5-80.2,178.7-178.7,178.7s-178.7-80.2-178.7-178.7s80.2-178.7,178.7-178.7 S381.9,104.65,381.9,203.25z"
-                                            ></path>
-                                        </g>
-                                    </g>
-                                </g>
-                            </svg>
-                        </div>
-                        <div class="i"></div>
-                        <div class="i"></div>
-                        <div class="i"></div>
-                        <div class="i"></div>
-                        <span></span>
-                    </div>
-                </div>
 
 
                 {/* Desktop Navigation */}
